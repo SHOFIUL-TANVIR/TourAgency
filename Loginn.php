72710ec15b9867dd1206bb97d6872e4d7b461ea2
SHOFIUL-TANVIR <90600284+SHOFIUL-TANVIR@users.noreply.github.com>
2021-12-14 21:23:33 +0600

    Add files via upload

---
+++ Loginn.php
@@ -0,0 +1,49 @@
+
+<html>
+	<body>
+	<img width ="2370px"src="log1.jpg"></img>
+	<fieldset><h1>Log In</h1>
+	<form action="LoginUs.php" method="POST">
+	<table align = "Right">
+	<tr>
+		<td>
+		Username
+		</td>
+		<td>
+			<input type="text" name="username"  placeholder="User name" >
+		</td>
+		
+	</tr>
+	<tr>
+		<td>
+		Password
+		</td>
+		<td>
+			<input type="password" name="password" placeholder="Password" >
+		</td>
+		
+	</tr>
+	<tr>
+		<td colspan="2" align="center">
+			<a href="Forget Pass\forgetpass.php">Forget Password</a>
+		</td>
+	</tr>
+	<tr>
+		<td colspan="2" align="center">
+			<a href="Create Account\Create Account.php">
+			Create Account
+			</a>
+		</td>
+	</tr>
+	<tr>
+		<td colspan="2" align="center">
+			<input type="submit" value="Log In">
+		</td>
+	</tr>
+	</table>	
+	<form>
+	</fieldset>
+	<img width ="2370px"src="log.jpg"></img>
+	</body>
+
+</html>
